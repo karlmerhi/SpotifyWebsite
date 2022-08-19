cf17e4840c42bc9bd3766ae5b5435ca24e3a9dce
Karl Merhi <karlmerhi99@gmail.com>
2022-08-19 02:02:58 -0400

    asd

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get("x-forwarded-proto") !== "https") {
+    return res.redirect("https://" + req.get("host") + req.url);
+  }
+  next();
+}
+
 const express = require("express");
 const path = require("path");
 const app = express();
+app.use(requireHTTPS);
+app.use(express.static(__dirname + '\\dist\\web422-a4'));
+app.use(express.static(__dirname + '/angular-build'));
 
-app.use(express.static(__dirname + "/dist/<app-name>"));
+
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/<app-name>/index.html"));
+  res.sendFile(path.join(__dirname + '\\src\\index.html'));
 });
+
 app.listen(process.env.PORT || 8080);
+
